eef005197f886868d865d4f4979334b7ecca6498
Andrew Holzer <andrew.holzer@digilent.com>
2017-12-20 20:21:17 +0000

    Add second 'object' test to handle user confirmation prompt

--- spec/core.js
+++ spec/core.js
@@ -1,6 +1,13 @@
 global.AWS = require('aws-sdk');
 let awsTestCredentials = require('../awsTestCredentials.json');
 let DigilentAuthJs = require('../dist/digilent-auth-js.js').DigilentAuthJs;
+var prompt = require('syncprompt');
+
+// authenticating requires that a callback be given that returns a new password
+// in case the user is asked to change theirs upon logging in.
+function returnNewPassword() {
+    return awsTestCredentials.password;
+}
 
 describe("Digilent-Auth-JS core", () => {
 
@@ -23,6 +30,48 @@ describe("Digilent-Auth-JS core", () => {
         it('has a constructor that returns an instnace of the class', () => {
             let digilentAuthJs = new DigilentAuthJs();
             expect(digilentAuthJs).toBeDefined();
+        });
+    });
+    
+    // this is in a separeate "object" describe because it requires the test to
+    // 'pause' while the tester goes and confirms the user.
+    describe("object", () => {
+        let originalTimeout;
+        
+        beforeEach(() => {
+            originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        });
+
+        it('can successfully sign-up a user', (done) => {
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
+            let digilentAuthJs = new DigilentAuthJs();
+
+            digilentAuthJs.initialize(
+                awsTestCredentials.userPoolId,
+                awsTestCredentials.clientId,
+                awsTestCredentials.region,
+                awsTestCredentials.identityPoolId
+            );
+
+            digilentAuthJs.signUp(
+                'andrew.holzer@digilent.com',
+                awsTestCredentials.username,
+                awsTestCredentials.password
+            )
+            .then((success) => {
+                done();           
+            })
+            .catch((error) => {
+                expect(true).toBe(false);
+            });
+        });
+
+        afterEach(function () {
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+        });
+        
+        afterAll(() => {
+            prompt('\r\nPress Enter after confirming user');
         });
     });
 
@@ -55,13 +104,83 @@ describe("Digilent-Auth-JS core", () => {
         });
 
         it('can succesfully authenticate a user', (done) => {
-
             jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
+
             let digilentAuthJs = new DigilentAuthJs();
+            
             digilentAuthJs.initialize(awsTestCredentials.userPoolId, awsTestCredentials.clientId, awsTestCredentials.region, awsTestCredentials.identityPoolId);
-            digilentAuthJs.authenticateUser(awsTestCredentials.username, awsTestCredentials.password, () => {
-                return 'new password';
+            digilentAuthJs.authenticateUser(
+                awsTestCredentials.username,
+                awsTestCredentials.password,
+                returnNewPassword
+            )
+            .then((success) => {
+                expect(true).toBe(true);
+                done();
             })
+            .catch((e) => {
+                console.log(e);
+                expect(true).toBe(false);
+            })
+        });
+
+        it('can successfully change a user\'s password', (done) => {
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
+
+            let digilentAuthJs = new DigilentAuthJs();
+            let newPassword = "testpassword";
+            
+            digilentAuthJs.initialize(
+                awsTestCredentials.userPoolId, 
+                awsTestCredentials.clientId, 
+                awsTestCredentials.region, 
+                awsTestCredentials.identityPoolId
+            );
+            
+            digilentAuthJs.authenticateUser(
+                awsTestCredentials.username,
+                awsTestCredentials.password,
+                returnNewPassword
+            )
+            .then((success) => {
+                digilentAuthJs.changeAuthenticatedUserPassword(
+                    awsTestCredentials.password, newPassword
+                )
+                .then((success) => {
+                    awsTestCredentials.password = newPassword; // so that subsequent tests don't fail as a result of the password change
+                    expect(true).toBe(true);
+                    done();
+                })
+                .catch((e) => {
+                    console.log(e);
+                    expect(true).toBe(false);
+                });
+            })
+            .catch((e) => {
+                console.log(e);
+                expect(true).toBe(false);
+            });
+        });
+        
+        it('can succesfully sign-out a user', (done) => {
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
+
+            let digilentAuthJs = new DigilentAuthJs();
+            
+            digilentAuthJs.initialize(
+                awsTestCredentials.userPoolId, 
+                awsTestCredentials.clientId, 
+                awsTestCredentials.region, 
+                awsTestCredentials.identityPoolId
+            );
+
+            digilentAuthJs.authenticateUser(
+                awsTestCredentials.username,
+                awsTestCredentials.password,
+                returnNewPassword
+            )
+            .then((success) => {
+                digilentAuthJs.signOutAuthenticatedUser()
                 .then((success) => {
                     expect(true).toBe(true);
                     done();
@@ -69,13 +188,51 @@ describe("Digilent-Auth-JS core", () => {
                 .catch((e) => {
                     console.log(e);
                     expect(true).toBe(false);
+                });
+            })
+            .catch((e) => {
+                console.log(e);
+                expect(true).toBe(false);
+            });
+        });
+        
+        it('can successfully delete a user', (done) =>{
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
+
+            let digilentAuthJs = new DigilentAuthJs();
+            
+            digilentAuthJs.initialize(
+                awsTestCredentials.userPoolId, 
+                awsTestCredentials.clientId, 
+                awsTestCredentials.region, 
+                awsTestCredentials.identityPoolId
+            );
+
+            digilentAuthJs.authenticateUser(
+                awsTestCredentials.username,
+                awsTestCredentials.password,
+                returnNewPassword
+            )
+            .then((success) => {
+                digilentAuthJs.deleteAuthenticatedUser()
+                .then((success) => {
+                    expect(true).toBe(true);
+                    done();
                 })
+                .catch((e) => {
+                    console.log(e);
+                    expect(true).toBe(false);
+                });
+            })
+            .catch((e) => {
+                console.log(e);
+                expect(true).toBe(false);
+            });
         });
 
         afterEach(function () {
             jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
         });
-
-    });
+    });    
 });
 
